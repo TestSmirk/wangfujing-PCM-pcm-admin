e81f3c8c8f832ff06c33cc5ce55f365d68520ae2
wangxuan <1160880871@qq.com>
2016-08-29 10:17:41 +0800

    门店信息表，新增门店信息功能。

--- src/main/java/com/wangfj/product/core/controller/support/PcmOrgPara.java
+++ src/main/java/com/wangfj/product/core/controller/support/PcmOrgPara.java
@@ -2,144 +2,309 @@ package com.wangfj.product.core.controller.support;
 
 public class PcmOrgPara {
 
-	private Long sid;
+    private Long sid;
 
-	private String parentSid;/* 所属上级sid */
+    private String parentSid;/* 所属上级sid */
 
-	private Long groupSid;/* 所属集团sid */
+    private Long groupSid;/* 所属集团sid */
 
-	private String organizationCode;/* 机构编码 */
+    private String organizationCode;/* 机构编码 */
 
-	private String organizationName;/* 机构名称 */
+    private String organizationName;/* 机构名称 */
 
-	private Integer organizationType;/* 机构类别 */
+    private Integer organizationType;/* 机构类别 */
 
-	private Integer organizationStatus;/* 机构状态 */
+    private Integer organizationStatus;/* 机构状态 */
 
-	private Integer storeType;/* 门店类型 */
+    private Integer storeType;/* 门店类型 */
 
-	private String shippingPoint;/* 集货地点编码 */
+    private String shippingPoint;/* 集货地点编码 */
 
-	private String areaCode;/* 门店所属城市在省市区字典中的编码 */
+    private String areaCode;/* 门店所属城市在省市区字典中的编码 */
 
-	private String createName; /* 创建人 */
+    private String createName; /* 创建人 */
 
-	private String updateName; /* 修改人 */
+    private String updateName; /* 修改人 */
 
-	private String actionCode;
+    private String actionCode;
 
-	public Long getSid() {
-		return sid;
-	}
+    /**
+     * 门店信息(新加表字段)
+     */
+    private String registeredAddress;//注册地址
 
-	public void setSid(Long sid) {
-		this.sid = sid;
-	}
+    private String postCode;//邮编
 
-	public String getParentSid() {
-		return parentSid;
-	}
+    private String legalRepresentative;//法定代表人
 
-	public void setParentSid(String parentSid) {
-		this.parentSid = parentSid;
-	}
+    private String agent;//委托代理人
 
-	public Long getGroupSid() {
-		return groupSid;
-	}
+    private String taxRegistrationNumber;//税务登记号
 
-	public void setGroupSid(Long groupSid) {
-		this.groupSid = groupSid;
-	}
+    private String bank;//开户行
 
-	public String getOrganizationCode() {
-		return organizationCode;
-	}
+    private String bankAccount;//开户行账号
 
-	public void setOrganizationCode(String organizationCode) {
-		this.organizationCode = organizationCode;
-	}
+    private String telephoneNumber;//电话(区号+座机)
 
-	public String getOrganizationName() {
-		return organizationName;
-	}
+    private String faxNumber;//传真(区号+座机)
 
-	public void setOrganizationName(String organizationName) {
-		this.organizationName = organizationName;
-	}
+    private String field1;//预留字段
 
-	public Integer getOrganizationType() {
-		return organizationType;
-	}
+    private String field2;//预留字段
 
-	public void setOrganizationType(Integer organizationType) {
-		this.organizationType = organizationType;
-	}
+    private String field3;//预留字段
 
-	public Integer getOrganizationStatus() {
-		return organizationStatus;
-	}
+    private String field4;//预留字段
 
-	public void setOrganizationStatus(Integer organizationStatus) {
-		this.organizationStatus = organizationStatus;
-	}
+    private String field5;//预留字段
 
-	public Integer getStoreType() {
-		return storeType;
-	}
+    public Long getSid() {
+        return sid;
+    }
 
-	public void setStoreType(Integer storeType) {
-		this.storeType = storeType;
-	}
+    public void setSid(Long sid) {
+        this.sid = sid;
+    }
 
-	public String getShippingPoint() {
-		return shippingPoint;
-	}
+    public String getParentSid() {
+        return parentSid;
+    }
 
-	public void setShippingPoint(String shippingPoint) {
-		this.shippingPoint = shippingPoint;
-	}
+    public void setParentSid(String parentSid) {
+        this.parentSid = parentSid;
+    }
 
-	public String getAreaCode() {
-		return areaCode;
-	}
+    public Long getGroupSid() {
+        return groupSid;
+    }
 
-	public void setAreaCode(String areaCode) {
-		this.areaCode = areaCode;
-	}
+    public void setGroupSid(Long groupSid) {
+        this.groupSid = groupSid;
+    }
 
-	public String getCreateName() {
-		return createName;
-	}
+    public String getOrganizationCode() {
+        return organizationCode;
+    }
 
-	public void setCreateName(String createName) {
-		this.createName = createName;
-	}
+    public void setOrganizationCode(String organizationCode) {
+        this.organizationCode = organizationCode;
+    }
 
-	public String getUpdateName() {
-		return updateName;
-	}
+    public String getOrganizationName() {
+        return organizationName;
+    }
 
-	public void setUpdateName(String updateName) {
-		this.updateName = updateName;
-	}
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
 
-	public String getActionCode() {
-		return actionCode;
-	}
+    public Integer getOrganizationType() {
+        return organizationType;
+    }
 
-	public void setActionCode(String actionCode) {
-		this.actionCode = actionCode;
-	}
+    public void setOrganizationType(Integer organizationType) {
+        this.organizationType = organizationType;
+    }
 
-	@Override
-	public String toString() {
-		return "PcmOrgPara [sid=" + sid + ", parentSid=" + parentSid + ", groupSid=" + groupSid
-				+ ", organizationCode=" + organizationCode + ", organizationName="
-				+ organizationName + ", organizationType=" + organizationType
-				+ ", organizationStatus=" + organizationStatus + ", storeType=" + storeType
-				+ ", shippingPoint=" + shippingPoint + ", areaCode=" + areaCode + ", createName="
-				+ createName + ", updateName=" + updateName + ", actionCode=" + actionCode + "]";
-	}
+    public Integer getOrganizationStatus() {
+        return organizationStatus;
+    }
 
+    public void setOrganizationStatus(Integer organizationStatus) {
+        this.organizationStatus = organizationStatus;
+    }
+
+    public Integer getStoreType() {
+        return storeType;
+    }
+
+    public void setStoreType(Integer storeType) {
+        this.storeType = storeType;
+    }
+
+    public String getShippingPoint() {
+        return shippingPoint;
+    }
+
+    public void setShippingPoint(String shippingPoint) {
+        this.shippingPoint = shippingPoint;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName;
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
+    public void setActionCode(String actionCode) {
+        this.actionCode = actionCode;
+    }
+
+    public String getRegisteredAddress() {
+        return registeredAddress;
+    }
+
+    public void setRegisteredAddress(String registeredAddress) {
+        this.registeredAddress = registeredAddress;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getLegalRepresentative() {
+        return legalRepresentative;
+    }
+
+    public void setLegalRepresentative(String legalRepresentative) {
+        this.legalRepresentative = legalRepresentative;
+    }
+
+    public String getAgent() {
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
+    }
+
+    public String getTaxRegistrationNumber() {
+        return taxRegistrationNumber;
+    }
+
+    public void setTaxRegistrationNumber(String taxRegistrationNumber) {
+        this.taxRegistrationNumber = taxRegistrationNumber;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+    public String getField4() {
+        return field4;
+    }
+
+    public void setField4(String field4) {
+        this.field4 = field4;
+    }
+
+    public String getField5() {
+        return field5;
+    }
+
+    public void setField5(String field5) {
+        this.field5 = field5;
+    }
+
+    @Override
+    public String toString() {
+        return "PcmOrgPara{" +
+                "sid=" + sid +
+                ", parentSid='" + parentSid + '\'' +
+                ", groupSid=" + groupSid +
+                ", organizationCode='" + organizationCode + '\'' +
+                ", organizationName='" + organizationName + '\'' +
+                ", organizationType=" + organizationType +
+                ", organizationStatus=" + organizationStatus +
+                ", storeType=" + storeType +
+                ", shippingPoint='" + shippingPoint + '\'' +
+                ", areaCode='" + areaCode + '\'' +
+                ", createName='" + createName + '\'' +
+                ", updateName='" + updateName + '\'' +
+                ", actionCode='" + actionCode + '\'' +
+                ", registeredAddress='" + registeredAddress + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", legalRepresentative='" + legalRepresentative + '\'' +
+                ", agent='" + agent + '\'' +
+                ", taxRegistrationNumber='" + taxRegistrationNumber + '\'' +
+                ", bank='" + bank + '\'' +
+                ", bankAccount='" + bankAccount + '\'' +
+                ", telephoneNumber='" + telephoneNumber + '\'' +
+                ", faxNumber='" + faxNumber + '\'' +
+                ", field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                ", field4='" + field4 + '\'' +
+                ", field5='" + field5 + '\'' +
+                '}';
+    }
 }
